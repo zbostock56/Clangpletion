4f57f2f53c3d2b48726910539fd160d3922af815
Jack Saysana <saysanabjack@gmail.com>
2022-02-12 14:55:46 -0500

    Fixed clang-based memory leaks, revamped memory allocation, sped up completions

--- src/clangpletion.c
+++ src/clangpletion.c
@@ -3,153 +3,82 @@
 #include <stdlib.h>
 #include "clangpletion.h"
 
+char *g_plugin_loc = NULL;
+size_t g_plugin_loc_max = PLUGIN_LOC_INIT_MAX;
+size_t g_plugin_loc_len = 0;
+
+char *g_filename = NULL;
+size_t g_filename_max = FILENAME_INIT_MAX;
+size_t g_filename_len = 0;
+
+int g_row = -1;
+int g_col = -1;
+
+char *g_token = NULL;
+size_t g_token_max = TOKEN_INIT_MAX;
+size_t g_token_len = 0;
+
+char *g_contents = NULL;
+size_t g_contents_max = CONTENTS_INIT_MAX;
+size_t g_contents_len = 0;
+
+char recommendations[COMP_MAX];
+
+CXIndex g_index = NULL;
+CXTranslationUnit g_unit = NULL;
+
 char *complete(char *location) {
-  // Reserve memory on the heap for path of the plugin
-  char *plugin_loc = (char *) malloc(PLUGIN_LOC_INIT_MAX);
-  size_t plugin_loc_max = PLUGIN_LOC_INIT_MAX;
-  size_t plugin_loc_len = 0;
-  // Reserve memory on the heap for name of file that will be analyzed
-  char *file = (char *) malloc(FILENAME_INIT_MAX);
-  size_t file_max = FILENAME_INIT_MAX;
-  size_t file_len = 0;
-  // Reserve memory for the row the user is currently at
-  char row[20] = "";
-  // Reserve memory for the column the user is currently at
-  char col[20] = "";
-  // Reserve memory for the current token the user has typed
-  static char wrd[100];
-  // Reserve memory on the heap for the contents of the file the user is
-  // currently editing
-  char *contents = (char *) malloc(CONTENTS_INIT_MAX);
-  size_t contents_max = CONTENTS_INIT_MAX;
-  size_t contents_len = 0;
-
-  // String that stores all the completion recommendations that will be fed
-  // to the user
-  static char recommendations[COMP_MAX];
-
-  for (int i = 0; i < COMP_MAX; i++) {
-    if (i < 100) {
-      wrd[i] = '\0';
-    }
-    recommendations[i] = '\0';
+  int pop_result = populate_args(location);
+  if (pop_result) {
+    return "FAILED TO POPULATE ARGUMENTS";
   }
 
-  char *args[] = { PLUGIN_LOC, FILENAME, row, col, WORD, CONTENTS };
-
-  // Populate arguments from location string
-  char next_char = '\0';
-  int i = 0;
-  int pos = 0;
-  int arg = ARG_PLUGIN;
-
-  while (arg < NUM_ARGS) {
-    next_char = location[i];
-    if ((next_char == '\n' && arg < ARG_CONTENT) || next_char == '\0') {
-      args[arg][pos] = '\0';
-      arg++;
-      pos = 0;
-      i++;
-      continue;
-    }
-
-    // Process arguments that will be stored on the heap
-    if (arg == ARG_PLUGIN || arg == ARG_FILE || arg == ARG_CONTENT) {
-      args[arg][pos] = next_char;
-      pos++;
-
-      if (arg == ARG_PLUGIN) {
-        plugin_loc_len++;
-        if (plugin_loc_len == plugin_loc_max) {
-          plugin_loc_max *= 2;
-          PLUGIN_LOC = (char *) realloc(PLUGIN_LOC, plugin_loc_max);
-          if (PLUGIN_LOC == NULL) {
-            free((void *) FILENAME);
-            free((void *) CONTENTS);
-            return "FAILED TO ALLOCATE PLUGIN LOCATION";
-          }
-        }
-      }
-      else if (arg == ARG_FILE) {
-        file_len++;
-        if (file_len == file_max) {
-          file_max *= 2;
-          FILENAME = (char *) realloc(FILENAME, file_max);
-          if (file == NULL) {
-            free((void *) PLUGIN_LOC);
-            free((void *) CONTENTS);
-            return "FAILED TO ALLOCATE FILENAME";
-          }
-        }
-      }
-      else if (arg == ARG_CONTENT) {
-        contents_len++;
-        if (contents_len == contents_max) {
-          contents_max *= 2;
-          CONTENTS = (char *) realloc(CONTENTS, contents_max);
-          if (CONTENTS == NULL) {
-            free((void *) PLUGIN_LOC);
-            free((void *) FILENAME);
-            return "FAILED TO ALLOCATE FILE CONTENTS";
-          }
-        }
-      }
-    }
-    // Process arguments that will be stored on the stack
-    else {
-      args[arg][pos] = next_char;
-      pos++;
-    }
-    i++;
-  }
-
-  char *debug_path = (char *) malloc(plugin_loc_len + 19);
+  char *debug_path = (char *) malloc(g_plugin_loc_len + 19);
   sprintf(debug_path, "%s/src/debug_log.txt", PLUGIN_LOC);
 
   FILE *debug_log = fopen(debug_path, "w");
   free((void *) debug_path);
   if (debug_log == NULL) {
-    free((void *) PLUGIN_LOC);
-    free((void *) FILENAME);
-    free((void *) CONTENTS);
+    free_allocated_memory();
 
     return "DEBUG FAILED";
   }
 
   fprintf(debug_log, "DEGUG INITIALIZED\n\n");
 
+  fprintf(debug_log, "Plugin Location: %s Buf Size: %lld\nFilename: %s Buf Size: %lld\nRow: %d\nCol: %d\nWord: %s Buf Size: %lld\n"
+          "========== CONTENTS ==========\n%s\n=========== CONTENTS END ==========\nBuf Size: %lld\n",
+          PLUGIN_LOC, g_plugin_loc_max, FILENAME, g_filename_max, ROW, COL, WORD, g_token_max, CONTENTS, g_contents_max);
 
-  fprintf(debug_log, "Plugin Location: %s\nFilename: %s\nRow: %d\nCol: %d\nWord: %s\n"
-          "========== CONTENTS ==========\n%s\n=========== CONTENTS END ==========\n",
-          PLUGIN_LOC, FILENAME, ROW, COL, WORD, CONTENTS);
+  fclose(debug_log);
 
   // Clang set-up
-  CXIndex index = clang_createIndex(0, 0);
-
-  CXTranslationUnit unit = clang_parseTranslationUnit(
-    index,
-    FILENAME,
-    0,
-    0,
-    0,
-    0,
-    clang_defaultCodeCompleteOptions()
-  );
-
-  if (unit == 0) {
-    free((void *) PLUGIN_LOC);
-    free((void *) FILENAME);
-    free((void *) CONTENTS);
-    fclose(debug_log);
-    debug_log = NULL;
-    return "NULL";
+  if (g_index == NULL) {
+    g_index = clang_createIndex(0, 0);
   }
 
-  struct CXUnsavedFile unsaved_file = { FILENAME, CONTENTS, contents_len };
+  if (g_unit == NULL) {
+    g_unit = clang_parseTranslationUnit(
+      g_index,
+      FILENAME,
+      0,
+      0,
+      0,
+      0,
+      clang_defaultCodeCompleteOptions()
+    );
+
+    if (g_unit == NULL) {
+      free_allocated_memory();
+      return "NULL";
+    }
+  }
+
+  struct CXUnsavedFile unsaved_file = { FILENAME, CONTENTS, g_contents_len };
 
   // Code Complete at the specified location
   CXCodeCompleteResults *comp_results = clang_codeCompleteAt(
-    unit,
+    g_unit,
     FILENAME,
     ROW,
     COL,
@@ -159,11 +88,7 @@ char *complete(char *location) {
   );
 
   if (comp_results == NULL) {
-    free((void *) PLUGIN_LOC);
-    free((void *) FILENAME);
-    free((void *) CONTENTS);
-    fclose(debug_log);
-    debug_log = NULL;
+    free_allocated_memory();
     return "Null";
   }
 
@@ -249,12 +174,181 @@ char *complete(char *location) {
   }
 
   clang_disposeCodeCompleteResults(comp_results);
-  free((void *) PLUGIN_LOC);
-  free((void *) FILENAME);
-  free((void *) CONTENTS);
-  fclose(debug_log);
-  debug_log = NULL;
+  free_allocated_memory();
   return recommendations;
+}
+
+int populate_args(char *arg_str) {
+  // Reset the recommendations string
+
+  for (int j = 0; j < COMP_MAX; j++) {
+    recommendations[j] = '\0';
+  }
+
+  int i = 0;
+
+  // Populate g_plugin_loc
+  g_plugin_loc_len = 0;
+  if (g_plugin_loc == NULL) {
+    g_plugin_loc = malloc(PLUGIN_LOC_INIT_MAX);
+    if (g_plugin_loc == NULL) {
+      free_allocated_memory();
+      return -1;
+    }
+  }
+  while (arg_str[i] != '\n') {
+    g_plugin_loc[g_plugin_loc_len] = arg_str[i];
+    g_plugin_loc_len++;
+    i++;
+    if (g_plugin_loc_len >= g_plugin_loc_max) {
+      g_plugin_loc_max *= 2;
+      g_plugin_loc = realloc(g_plugin_loc, g_plugin_loc_max);
+
+      if (g_plugin_loc == NULL) {
+        free_allocated_memory();
+        return -1;
+      }
+    }
+  }
+  g_plugin_loc[g_plugin_loc_len] = '\0';
+  i++;
+
+  // Populate g_filename
+  g_filename_len = 0;
+  if (g_filename == NULL) {
+    g_filename = malloc(FILENAME_INIT_MAX);
+    if (g_filename == NULL) {
+      free_allocated_memory();
+      return -1;
+    }
+  }
+  while (arg_str[i] != '\n') {
+    g_filename[g_filename_len] = arg_str[i];
+    g_filename_len++;
+    i++;
+    if (g_filename_len >= g_filename_max) {
+      g_filename_max *= 2;
+      g_filename = realloc(g_filename, g_filename_max);
+
+      if (g_filename == NULL) {
+        free_allocated_memory();
+        return -1;
+      }
+    }
+  }
+  g_filename[g_filename_len] = '\0';
+  i++;
+
+  // Populate g_row
+  char row_buf[20];
+  int row_len = 0;
+  while (arg_str[i] != '\n') {
+    row_buf[row_len] = arg_str[i];
+    row_len++;
+    i++;
+  }
+  row_buf[row_len] = '\0';
+  g_row = parse_int(row_buf);
+  i++;
+
+  // Populate g_col
+  char col_buf[20];
+  int col_len = 0;
+  while (arg_str[i] != '\n') {
+    col_buf[col_len] = arg_str[i];
+    col_len++;
+    i++;
+  }
+  col_buf[col_len] = '\0';
+  g_col = parse_int(col_buf);
+  i++;
+
+  // Populate g_token
+  g_token_len = 0;
+  if (g_token == NULL) {
+    g_token = malloc(TOKEN_INIT_MAX);
+    if (g_token == NULL) {
+      free_allocated_memory();
+      return -1;
+    }
+  }
+  while (arg_str[i] != '\n') {
+    g_token[g_token_len] = arg_str[i];
+    g_token_len++;
+    i++;
+    if (g_token_len >= g_token_max) {
+      g_token_max *= 2;
+      g_token = realloc(g_token, g_token_max);
+
+      if (g_token == NULL) {
+        free_allocated_memory();
+        return -1;
+      }
+    }
+  }
+  g_token[g_token_len] = '\0';
+  i++;
+
+  // Populate g_contents
+  g_contents_len = 0;
+  if (g_contents == NULL) {
+    g_contents = malloc(CONTENTS_INIT_MAX);
+    if (g_contents == NULL) {
+      free_allocated_memory();
+      return -1;
+    }
+  }
+  while (arg_str[i] != '\0') {
+    g_contents[g_contents_len] = arg_str[i];
+    g_contents_len++;
+    i++;
+    if (g_contents_len >= g_contents_max) {
+      g_contents_max *= 2;
+      g_contents = realloc(g_contents, g_contents_max);
+
+      if (g_contents == NULL) {
+        free_allocated_memory();
+        return -1;
+      }
+    }
+  }
+  g_contents[g_contents_len] = '\0';
+  i++;
+
+  return 0;
+}
+
+int free_allocated_memory(void) {
+  if (g_plugin_loc != NULL) {
+    free(g_plugin_loc);
+    g_plugin_loc = NULL;
+    g_plugin_loc_len = 0;
+  }
+  if (g_filename != NULL) {
+    free(g_filename);
+    g_filename = NULL;
+    g_filename_len = 0;
+  }
+  if (g_token != NULL) {
+    free(g_token);
+    g_token = NULL;
+    g_token_len = 0;
+  }
+  if (g_contents != NULL) {
+    free(g_contents);
+    g_contents = NULL;
+    g_contents_len = 0;
+  }
+  if (g_index != NULL) {
+    clang_disposeIndex(g_index);
+    g_index = NULL;
+  }
+  if (g_unit != NULL) {
+    clang_disposeTranslationUnit(g_unit);
+    g_unit = NULL;
+  }
+
+  return 0;
 }
 
 int parse_int(char *str) {
